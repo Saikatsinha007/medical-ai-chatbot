8d9ab9deab70482bc5e772593077cebe4c5401ad
Saikat Sinha <saikatsinha21@gmail.com>
2024-12-23 15:08:31 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Saikat Sinha',
+    author_email= 'saikatsinha21@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
